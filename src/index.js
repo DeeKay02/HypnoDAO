27edc2ecb13dda7cb7364e1258ffdb5e00e3e66f
A Dhruva <48171972+DeeKay02@users.noreply.github.com>
2022-11-10 23:11:16 +0530

    Update to Goerli

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// Import thirdweb provider and Rinkeby ChainId
+// Import thirdweb provider and Goerli ChainId
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Rinkeby;
+const activeChainId = ChainId.Goerli;
 
 // Wrap your app with the thirdweb provider
 ReactDOM.render(
